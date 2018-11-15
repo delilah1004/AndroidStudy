d7441ed6f9bc9bc01875bb1bece535c34ee71c5d
Delilah <pskdy8414@gmail.com>
2018-11-15 16:30:56 +0900

    20181115 Study

---
+++ app/src/main/java/com/appcenter_d/delilahapplication/ScheduleActivity.java
@@ -0,0 +1,14 @@
+package com.appcenter_d.delilahapplication;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+public class ScheduleActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule);
+    }
+}
